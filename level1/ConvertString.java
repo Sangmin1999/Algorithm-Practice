a0e8191f327b862ccc5cfe7a2f559720193972f9
Sangmin Lee <sart5534@gmail.com>
2024-08-08 09:52:01 +0900

    문자열을 정수로 바꾸기

---
+++ level1/ConvertString.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class ConvertString {
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            return answer = Integer.parseInt(s); // 문자열을 정수로 바꾼다
+        }
+    }
+}
