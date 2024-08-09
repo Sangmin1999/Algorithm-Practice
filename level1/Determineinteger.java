3bf361fefa4c2692dd38ad9b1bc92f5777f5c43b
Sangmin Lee <sart5534@gmail.com>
2024-08-09 09:44:07 +0900

    Level1 - 정수 제곱근 판별

---
+++ level1/Determineinteger.java
@@ -0,0 +1,23 @@
+package level1;
+
+public class Determineinteger {
+
+    class Solution {
+        public long solution(long n) {
+            long answer = 0;
+
+            while (answer * answer < n) {
+                answer++;
+            }
+            if (answer * answer == n) {
+                answer++;
+                answer *= answer;
+            } else {
+                answer = -1;
+            }
+            return answer;
+        }
+    }
+
+
+}
