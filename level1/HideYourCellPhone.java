2db9b69a4ae874acf5fd38a312ba43eff9371d0f
Sangmin Lee <sart5534@gmail.com>
2024-08-19 11:24:50 +0900

    핸드폰 번호 가리기

---
+++ level1/HideYourCellPhone.java
@@ -0,0 +1,18 @@
+package level1;
+
+public class HideYourCellPhone {
+
+    class Solution {
+        public String solution(String phone_number) {
+            String answer = "";
+            for (int i = 0; i < phone_number.length(); i++) {
+                if (i < phone_number.length() - 4) {
+                    answer += "*";
+                } else {
+                    answer += phone_number.charAt(i);
+                }
+            }
+            return answer;
+        }
+    }
+}
