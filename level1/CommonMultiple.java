47d2d14b1811e48a9f7f2d1321e5f43e7b5f5f19
Sangmin Lee <sart5534@gmail.com>
2024-10-20 18:28:39 +0900

    공배수

---
+++ level1/CommonMultiple.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class CommonMultiple {
+
+    class Solution {
+        public int solution(int number, int n, int m) {
+            if (number % n == 0 && number % m == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
